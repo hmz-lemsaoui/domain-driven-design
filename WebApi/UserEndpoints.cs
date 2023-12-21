f1cf7bbe15c525a70ec996314f1e8714ebe97848
Hamza Lemsaoui <hamzalemsaoui@gmail.com>
2023-12-21 20:06:54 +0100

    first commit

---
+++ WebApi/UserEndpoints.cs
@@ -0,0 +1,16 @@
+﻿namespace WebApi;
+
+public static class UserEndpoints
+{
+
+    public static void MapUsrEndpoints(this IEndpointRouteBuilder app)
+    {
+        app.MapPost("api/users", async (CreateUserCommand command, ISender sender) =>
+        {
+            Result<Guid> result = await sender.Send(command);
+
+            return result.IsSuccess ? Results.Ok(result.Value) : Results.BadRequest();
+        });
+
+    }
+}
